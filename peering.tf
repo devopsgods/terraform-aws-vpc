c33452db56fadd0b670a7728c4ced82a78e25057
Devils <devopsdevil9gmail.com>
2026-04-04 13:27:28 +0100

    attempt2

--- peering.tf
+++ peering.tf
@@ -13,9 +13,37 @@ resource "aws_vpc_peering_connection" "default" {
         allow_remote_vpc_dns_resolution = true
     }
   tags = merge(
-    local.commom_tags,
+    local.common_tags,
      {
         Name = "${var.project}-${var.enviornment}-default"
     }
   )
+}
+
+resource "aws_route" "public_peering" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = data.aws.vpc.default.cidr_block
+  vpc_peering_connection             = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = data.aws.vpc.default.cidr_block
+  vpc_peering_connection             = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.database.id
+  destination_cidr_block = data.aws.vpc.default.cidr_block
+  vpc_peering_connection             = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "default" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.default.id
+  destination_cidr_block = var.vpc_cidr
+  vpc_peering_connection             = aws_vpc_peering_connection.default[count.index].id
 }
